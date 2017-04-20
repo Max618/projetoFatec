53831525527fea9ffffb1ccbaadb07a9ea038d47
Gabriel Santos <gabmaxsan@gmail.com>
2017-04-19 21:26:56 -0300

    server pra pc

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,118 +1,118 @@
-<?php
-
-namespace App\Http\Controllers\Auth;
-
-use App\User;
-use App\Social;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Socialite;
-use Illuminate\Http\Request;
-
-class RegisterController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
-
-
-    public function redirectToProvider($provider = null)
-    {
-        
-        return Socialite::driver($provider)->redirect();
-    }
-
-    public function handleProviderCallback(Request $request, $provider)
-    {
-        try
-        {
-            $socialUser = Socialite::driver($provider)->setHttpClient(new \GuzzleHttp\Client(['verify' => false]))->user();
-        }
-        catch(\Exception $e)
-        {
-            return redirect('/');
-        }
-
-        $socialProvider = Social::where('provider_id', $socialUser->getId())->first();
-        if(!$socialProvider)
-        {
-            $user = User::firstOrCreate(
-                [
-                'email' => $socialUser->getEmail(),
-                'name' => $socialUser->getName(),
-                'avatar' => $socialUser->getAvatar(),
-                ]);
-            $user->social()->create(
-                [
-                'provider_id' => $socialUser->getId(),
-                'provider' => $provider,
-                ]);
-            //$user->avatar = $socialUser->getAvatar();
-            //$user->save();
-        }
-        else 
-            $user = $socialProvider->user;
-
-        auth()->login($user);
-        session(['socialUser' => $socialUser]);
-       // session()->generate('socialUser') = $socialUser->getAvatar();
-        return redirect('/home');
-    }
-}
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Social;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Socialite;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+
+
+    public function redirectToProvider($provider = null)
+    {
+        
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback(Request $request, $provider)
+    {
+        try
+        {
+            $socialUser = Socialite::driver($provider)->user();
+        }
+        catch(\Exception $e)
+        {
+            return redirect('/');
+        }
+
+        $socialProvider = Social::where('provider_id', $socialUser->getId())->first();
+        if(!$socialProvider)
+        {
+            $user = User::firstOrCreate(
+                [
+                'email' => $socialUser->getEmail(),
+                'name' => $socialUser->getName(),
+                'avatar' => $socialUser->getAvatar(),
+                ]);
+            $user->social()->create(
+                [
+                'provider_id' => $socialUser->getId(),
+                'provider' => $provider,
+                ]);
+            //$user->avatar = $socialUser->getAvatar();
+            //$user->save();
+        }
+        else 
+            $user = $socialProvider->user;
+
+        auth()->login($user);
+        session(['socialUser' => $socialUser]);
+       // session()->generate('socialUser') = $socialUser->getAvatar();
+        return redirect('/home');
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,47 +1,52 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App;
-
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $user = auth()->user();
-        $projetos = $user->projetos;
-        $curtidas = App\Like::where('user_id',$user->id)->where('like', 1)->get();
-        $execucoes = $user->execucao;
-        foreach ($execucoes as $execucao) {
-            $projetos_ex[] = App\Projeto::find($execucao['projeto_id']);
-        }
-        $projetos_ex = array_unique($projetos_ex);
-        foreach ($projetos as $projeto) {
-            foreach ($projeto->execucao as $ex) {
-                $execucoes_projetos[] = $ex;
-                //$users[] = $ex->user['name'];
-                //dd($ex->projeto()->first());
-                //dd($execucoes_projetos);
-                //dd($ex->projeto()->pluck('name'));
-            }
-        }
-        //dd($users);
-        return view('home')->with(compact('projetos','curtidas','projetos_ex','execucoes_projetos'));
-    }
-}
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = auth()->user();
+        if(count($user->projetos) > 0)
+        {
+            $projetos = $user->projetos;
+            $curtidas = App\Like::where('user_id',$user->id)->where('like', 1)->get();
+            $execucoes = $user->execucao;
+            foreach ($execucoes as $execucao) {
+                $projetos_ex[] = App\Projeto::find($execucao['projeto_id']);
+            }
+            $projetos_ex = array_unique($projetos_ex);
+            foreach ($projetos as $projeto) {
+                foreach ($projeto->execucao as $ex) {
+                    $execucoes_projetos[] = $ex;
+                }
+            }
+        }
+        else
+        {
+            $projetos = null;
+            $curtidas = null;
+            $projetos_ex = null;
+            $execucoes_projetos = null;
+        }
+        return view('home')->with(compact('projetos','curtidas','projetos_ex','execucoes_projetos'));
+    }
+}
